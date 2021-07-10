9e5793d148e8c4b2f7190e776384ebd314547f9f
KevinRosado <kevin.rosado.s@uni.pe>
2021-07-10 16:37:32 -0500

    back-created

---
+++ inenapp/src/main/java/com/inen/inenapp/InenappApplication.java
@@ -0,0 +1,13 @@
+package com.inen.inenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InenappApplication.class, args);
+	}
+
+}
---
+++ inenapp/src/test/java/com/inen/inenapp/InenappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inen.inenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
